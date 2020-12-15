6cc524bf4b22912ae7dbcf06ad8a4c9b16b6d48b
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2020-12-15 18:56:06 +0300

    Refactor edit avatar option - useRef

--- src/components/EditAvatarPopup.js
+++ src/components/EditAvatarPopup.js
@@ -2,35 +2,27 @@ import React from 'react';
 
 import PopupWithForm from "./PopupWithForm";
 
-import { CurrentUserContext } from '../contexts/CurrentUserContext.js';
-
 
 function EditAvatarPopup(props) {
-  
-  const currentUserInfo = React.useContext(CurrentUserContext);
 
-  const [avatar, setAvatar] = React.useState('');
+  const avatarRef = React.useRef(0);
 
-  function handleAvatarChange(e) {
-    setAvatar(e.target.value);
+  function handleAvatarChange() {
+    avatarRef.current.focus();
   }
 
   function handleSubmit(e) {
     e.preventDefault();
     props.onUpdateAvatar({
-      avatar
+      avatar: avatarRef.current.value
     });
   }
-
-  React.useEffect(() => {
-    setAvatar(currentUserInfo.avatar);
-  }, []);
 
   return (
     <PopupWithForm isOpen={props.isOpen} onSubmit={handleSubmit} onClose={props.onClose} name="load-avatar" title="Обновить аватар" buttonTitle="Сохранить" children={
       <>
         <div className="popup__wrap">
-          <input required id="avatar-link-input" type="url" value={avatar} onChange={handleAvatarChange} className="popup__input popup__avatar-link" name="avatar" placeholder="Ссылка на картинку" />
+          <input required id="avatar-link-input" type="url" ref={avatarRef} onChange={handleAvatarChange} className="popup__input popup__avatar-link" name="avatar" placeholder="Ссылка на картинку" />
           <span id="avatar-link-input-error" className="popup__error-text popup__error-text_avatar-link"></span>
         </div>
       </>
