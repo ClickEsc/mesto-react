6689411b0c177029874901944b70ba916b232e88
Irina Skubilina-Burtseva <zebra-head@yandex.ru>
2020-12-17 00:08:04 +0300

    Refactor EditProfilePopup component

--- src/components/EditProfilePopup.js
+++ src/components/EditProfilePopup.js
@@ -8,8 +8,8 @@ function EditProfilePopup(props) {
   
   const currentUserInfo = React.useContext(CurrentUserContext);
 
-  const [name, setName] = React.useState(currentUserInfo.name);
-  const [description, setDescription] = React.useState(currentUserInfo.about);
+  const [name, setName] = React.useState('');
+  const [description, setDescription] = React.useState('');
 
   function handleNameChange(e) {
     setName(e.target.value);
@@ -28,19 +28,21 @@ function EditProfilePopup(props) {
   }
 
   React.useEffect(() => {
-    setName(currentUserInfo.name);
-    setDescription(currentUserInfo.about);
-  }, [currentUserInfo]);
+    if (currentUserInfo) {
+      setName(currentUserInfo.name);
+      setDescription(currentUserInfo.about);
+    }
+  }, [currentUserInfo, props.isOpen]);
 
   return (
     <PopupWithForm isOpen={props.isOpen} onSubmit={handleSubmit} onClose={props.onClose} name="edit-profile" title="Редактировать профиль" buttonTitle="Сохранить" children={
       <>
         <div className="popup__wrap">
-          <input required id="username-input" defaultValue={name} onChange={handleNameChange} className="popup__input popup__username" name="name" placeholder="Имя" minLength="2" maxLength="40" />
+          <input required id="username-input" value={ name || ''} onChange={handleNameChange} className="popup__input popup__username" name="name" placeholder="Имя" minLength="2" maxLength="40" />
           <span id="username-input-error" className="popup__error-text popup__error-text_username"></span>
         </div>
         <div className="popup__wrap">
-          <input required id="bio-input" defaultValue={description} onChange={handleDescriptionChange} className="popup__input popup__bio" name="about" placeholder="О себе" minLength="2" maxLength="200" />
+          <input required id="bio-input" value={description || ''} onChange={handleDescriptionChange} className="popup__input popup__bio" name="about" placeholder="О себе" minLength="2" maxLength="200" />
           <span id="bio-input-error" className="popup__error-text popup__error-text_bio"></span>
         </div>
       </>
